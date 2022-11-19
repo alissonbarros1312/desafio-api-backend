35d7714c007c9bd1b84426c43d5185219f06bbe9
Alisson <barrosalisson1312@gmail.com>
2022-11-18 22:09:04 -0300

    acrescentando teste de contrato

--- cypress/api/wooComerce/productReview.cy.js
+++ cypress/api/wooComerce/productReview.cy.js
@@ -4,65 +4,45 @@ import putProductReview from '../../contratos/putProductReviewSchema'
 import deleteProductReview from '../../contratos/deleteProductReviewSchema'
 import body from '../../fixtures/bodyProductReview.json'
 
-describe('Product Review', ()=>{
+describe('Product Review', () => {
 
-    it('Create Product Review - Aceite', ()=>{
-        cy.postProductReview().should((response)=>{
+    it('Create Product Review', () => {
+        cy.postProductReview().should((response) => {
             expect(response.status).to.be.eq(201)
             expect(response.body.reviewer_email).to.be.equal(body.productReviewValido.reviewer_email)
             expect(response.body.product_id).to.equal(body.productReviewValido.product_id)
             expect(response.body.rating).to.equal(body.productReviewValido.rating)
+            productReviewSchema.validateAsync(response.body)
             cy.deleteProductReview(response.body.id)
         })
     })
 
-    it('Create Product Review - Contrato', ()=>{
-        cy.postProductReview().should((response)=>{
-            return productReviewSchema.validateAsync(response.body)
-        })
-    })
-
-    it('Update Product Review - Aceite', ()=>{
-        cy.postProductReview().should((response)=>{
-            cy.putProductReview(response.body.id, 'João Souza').should((putResponse)=>{
+    it('Update Product Review', () => {
+        var atualizarNome = 'João Souza'
+        cy.postProductReview().should((response) => {
+            cy.putProductReview(response.body.id, atualizarNome).should((putResponse) => {
                 expect(putResponse.status).to.be.eq(200)
-                expect(putResponse.body.reviewer).to.be.equal('João Souza')
+                expect(putResponse.body.reviewer).to.be.equal(atualizarNome)
                 expect(response.body.reviewer_email).to.be.equal(body.productReviewValido.reviewer_email)
                 expect(response.body.product_id).to.equal(body.productReviewValido.product_id)
                 expect(response.body.rating).to.equal(body.productReviewValido.rating)
-                cy.deleteProductReview(response.body.id)
+                putProductReview.validateAsync(putResponse.body),
+                    cy.deleteProductReview(response.body.id)
             })
         })
     })
 
-    it('Update Product Review - Contrato', ()=>{
-        cy.postProductReview().should((response)=>{
-            cy.putProductReview(response.body.id, 'Maria Souza').should((putResponse)=>{
-                return putProductReview.validateAsync(putResponse.body)
-            })
-        })
-    })
-
-    it('Delete Product Review - Aceite', ()=>{
-        cy.postProductReview().should((response)=>{
-            cy.deleteProductReview(response.body.id).should((deleteResponse)=>{
+    it.only('Delete Product Review', () => {
+        cy.postProductReview().should((response) => {
+            cy.deleteProductReview(response.body.id).should((deleteResponse) => {
                 expect(deleteResponse.status).to.be.eq(200)
                 expect(deleteResponse.body.deleted).to.be.true
                 expect(deleteResponse.body.previous).to.exist
                 expect(deleteResponse.body.previous.reviewer_email).to.be.equal(body.productReviewValido.reviewer_email)
                 expect(deleteResponse.body.previous.product_id).to.equal(body.productReviewValido.product_id)
+                deleteProductReview.validateAsync(deleteResponse.body)
             })
         })
     })
-    
-    it('Delete Product Review - Contrato', ()=>{
-        cy.postProductReview().should((response)=>{
-            cy.deleteProductReview(response.body.id).should((deleteResponse)=>{
-                return deleteProductReview.validateAsync(deleteResponse.body)
-            })
-        })
-
-    })
-
 
 })
